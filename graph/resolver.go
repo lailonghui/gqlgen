96ec02738dc43986c2adbee1ae4fb7731b7c9a41
lailonghui <624758224@qq.com>
2020-11-01 13:03:54 +0800

    driver_info and driver_identity complete

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,8 +1,11 @@
 //go:generate go run github.com/99designs/gqlgen
 package graph
 
+import "github.com/jinzhu/gorm"
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+type Resolver struct {
+}
